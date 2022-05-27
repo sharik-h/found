ea448701c0a4204cba43cef0974128c9cb2187d6
Mohammed sharikh <mohammedsharik0123@gmail.com>
2022-05-27 11:50:40 +0530

    developed UI for LoginWithMob a page for loging in with mobile number

---
+++ app/src/main/java/com/example/found/LoginPages/LoginWith/LoginWithMob.kt
@@ -0,0 +1,96 @@
+package com.example.found.LoginPages.LoginWith
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun LoginWithMob() {
+
+    var name by remember{ mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFF78B3B3))
+    ) {
+        TopAppBar(
+            backgroundColor = Color.Transparent,
+            contentPadding = PaddingValues(start = 0.dp, end = 10.dp, top = 10.dp),
+            elevation = 0.dp,
+        ) {
+            TextButton(onClick = { /*TODO*/ }) {
+                Image(imageVector = Icons.Default.ArrowBack, contentDescription = "", colorFilter = ColorFilter.tint(
+                    Color.White), modifier = Modifier.size(30.dp))
+            }
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.height(50.dp).width(160.dp),
+                    shape = RoundedCornerShape(50),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                    elevation = ButtonDefaults.elevation(10.dp)
+                ) {
+                    Text(
+                        text = "SEND OTP",
+                        color = Color(0xFF78B3B3),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+
+                }
+            }
+
+        }
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 40.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Mobile Number",
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            TextField(
+                value = name,
+                onValueChange = { it -> name = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.White,
+                    unfocusedIndicatorColor = Color.White
+                )
+            )
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview1() {
+    LoginWithMob()
+}
