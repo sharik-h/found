47d7497ef84af20b9cec0aaa20909b4b8fd35d40
Mohammed sharikh <mohammedsharik0123@gmail.com>
2022-06-06 11:18:03 +0530

    Updated UI for LoginOptions page

--- app/src/main/java/com/example/found/LoginPages/login/LoginOptions.kt
+++ app/src/main/java/com/example/found/LoginPages/login/LoginOptions.kt
@@ -1,19 +1,20 @@
 package com.example.found.LoginPages.login
 
 import android.content.Intent
-import androidx.compose.foundation.BorderStroke
+import android.widget.Space
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,82 +25,92 @@ import androidx.navigation.compose.rememberNavController
 import com.example.found.LoginNavigation.Screen
 import com.example.found.LoginPages.LoginWith.LoginWithGoogle
 import com.example.found.R
-import kotlin.math.round
+import com.example.found.ui.theme.*
 
 @Composable
 fun LoginOptions(navHostController: NavHostController) {
 
-    var gclick = false
-    var text = "Continue with google"
+    val nunito_bold = Font(R.font.nunito_sans_bold)
+    val nunito_sans = Font(R.font.nunito_sans)
+    val context = LocalContext.current
 
     Column(
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF78B3B3))
+            .background(teritary90)
+            .padding(20.dp)
     ) {
-        TopAppBar(
-            backgroundColor = Color.Transparent,
-            elevation = 0.dp,
-            contentPadding = PaddingValues(start = 0.dp, end = 0.dp)
+        Text(
+            text = "Welcome to Found",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = teritary40,
+            fontFamily = FontFamily(nunito_bold)
+        )
+        Text(
+            text = "save any location you like, so you dont have to search for it again and again, and find the route with one click.",
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold,
+            color = teritary40,
+            fontFamily = FontFamily(nunito_sans)
+        )
+        Row(
+            modifier = Modifier.padding(vertical = 15.dp)
         ) {
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(
-                    text = "Log in ",
-                    textAlign = TextAlign.End,
-                    modifier = Modifier.fillMaxWidth(),
-                    fontSize = 20.sp,
-                    color = Color.White
-                )
-            }
-        }
-        Column(
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(15.dp),
-                modifier = Modifier.padding(20.dp)
+            Button(
+                onClick = { navHostController.navigate(route = Screen.enterUserDetails.route) },
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(45.dp)
+                    .fillMaxWidth()
+                    .weight(0.5f)
+                    .padding(end = 4.dp),
+                colors =  buttonColors(teritary40),
+                shape = RoundedCornerShape(30),
             ) {
                 Text(
-                    text = "Welcome to Found",
-                    fontSize = 40.sp,
+                    text = "Log In",
+                    fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White
+                    color = Color.White,
+                    fontFamily = FontFamily(nunito_sans)
                 )
-                Text(
-                    text = "Save a place that you like and make it easier to find the place when you wish to visit.",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-                val context = LocalContext.current
-                GoogleButton(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp)
-                        .padding(horizontal = 20.dp)
-                        .clip(RoundedCornerShape(50)),
-                ) { context.startActivity(Intent(context, LoginWithGoogle::class.java))}
-                Button(
-                    onClick = { navHostController.navigate(route = Screen.EnterUserDetails.route) },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp)
-                        .padding(horizontal = 20.dp),
-                    colors = buttonColors(backgroundColor = Color.Transparent),
-                    shape = RoundedCornerShape(50),
-                ) {
-                    Text(
-                        text = "Create Acccount",
-                        fontSize = 23.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    )
-                }
             }
+            GoogleButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .height(45.dp)
+                    .padding(start = 4.dp)
+                    .clip(RoundedCornerShape(30)),
+            ) { context.startActivity(Intent(context, LoginWithGoogle::class.java)) }
+        }
+        Divider(thickness = 1.dp)
+        Spacer(modifier = Modifier.height(15.dp))
+        Row() {
+            Text(
+                text = "If you don`t have and account you can signup here, ",
+                fontFamily = FontFamily(nunito_sans),
+                color = teritary40,
+                fontSize = 12.sp
+            )
+            Text(
+                text = "click here.",
+                textAlign = TextAlign.End,
+                fontSize = 12.sp,
+                color = teritary10,
+                fontFamily = FontFamily(nunito_sans),
+                modifier = Modifier
+                    .clickable {
+
+                    },
+            )
         }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
